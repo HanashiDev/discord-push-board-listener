70a8ac92b7fe5e10bf24b02fc91535fc55e4bf1e
Hanashi <hanashi@hanashi.eu>
2022-12-11 19:47:47 +0100

    better .d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,11 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            DISCORD_BOT_TOKEN: string;
-            DISCORD_GUILD_ID: string;
-            WOLTLAB_WEBHOOK_URL: string;
-            WOLTLAB_WEBHOOK_SECRET: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      DISCORD_BOT_TOKEN: string;
+      DISCORD_GUILD_ID: string;
+      WOLTLAB_WEBHOOK_URL: string;
+      WOLTLAB_WEBHOOK_SECRET: string;
     }
+  }
 }
 export {};
